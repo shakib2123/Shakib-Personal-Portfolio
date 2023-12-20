682535622ba52ed4a2a760f26cac8d6f8fac0beb
shakib2123 <mshakibalhasan21@gmail.com>
2023-12-20 18:50:46 +0600

    starBackground added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/main/Hero";
+import StarsCanvas from "./components/main/StarBackground";
 
 const App = () => {
   return (
     <main className="h-full w-full">
       <div className="flex  flex-col h-[850px] gap-20">
+        <StarsCanvas />
         <Hero />
       </div>
     </main>
